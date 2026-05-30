9ae67e4fa2c965d137e1bc4cf14aecf777fdc892
asish-t-babu-zaigo <ashish@zaigoinfotech.com>
2026-05-30 22:33:01 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def home():
+    return {
+        "message": "Welcome to FastAPI"
+    }
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    return {
+        "user_id": user_id,"name": f"User {user_id} this is a test for black so we can see if it works or not"
+    }
